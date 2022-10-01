bf513b03e876b0dee803e237aa3a1c7241fccea6
Miguel Velásquez <vmiguel.xy@gmail.com>
2022-10-01 15:29:10 -0400

    Detección del Regalito y Asención de Niveles

--- game.js
+++ game.js
@@ -1,21 +1,28 @@
 "use strict";
-
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+
+/* Initial Values */
 let canvasSize = null;
 let gridSize = null;
 let elementsSize = null;
-let playerPos = {
+const MAP_LIMITS = {
+	UP: 1,
+	LEFT: 1,
+	RIGHT: 10,
+	DOWN: 10
+};
+const playerPos = {
 	x: null,
 	y: null
 };
-let mapLimits = {
-	up: null,
-	left: null,
-	right: null,
-	down: null
-};
+const giftPos = {
+	x: null,
+	y: null
+}
+let actualLevel = 0;
 
+/* Event Listeners */
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 window.addEventListener("keydown", moveByKeys);
@@ -24,6 +31,7 @@ document.getElementById("left").addEventListener("click", moveByClicks);;
 document.getElementById("right").addEventListener("click", moveByClicks);;
 document.getElementById("down").addEventListener("click", moveByClicks);;
 
+/* Game Logic */
 function setCanvasSize() {
 	if (window.innerHeight < window.innerWidth) {
 		canvasSize = window.innerHeight * 0.8;
@@ -42,78 +50,94 @@ function renderMap() {
 	elementsSize = gridSize;
 	// console.log( {canvasSize, gridSize, elementsSize} );
 	
-	mapLimits.up = gridSize * 1;
-	mapLimits.left = gridSize * 0;
-	mapLimits.right = gridSize * 9;
-	mapLimits.down = gridSize * 10;
-	// console.log (mapLimits);
-	
 	game.textAlign = "start"
 	game.font = `${elementsSize}px sans-serif`;
 	
-	let actualMap = maps[0];
+	let actualMap = maps[actualLevel];
 	const mapRows = actualMap.trim().split("\n");
 	const mapElements = mapRows.map(row => row.trim().split(""));
 	
 	game.clearRect(0,0, canvasSize, canvasSize);
+	let firstLoad = (playerPos.x === null && playerPos.y === null);
+	let playerMoved = (playerPos.x !== null && playerPos.y !== null);
+	
 	mapElements.forEach((row, rowIndex) => {
 		row.forEach((col, colIndex) => {
 			const emoji = emojis[col];
-			const posX = colIndex * gridSize;
-			const posY = (rowIndex+1) * gridSize;
+			const posX = colIndex * gridSize; // 0 ~ 9
+			const posY = (rowIndex+1) * gridSize; // 1 ~ 10
 			game.fillText(emoji, posX, posY);
-			// console.log( {rowIndex, colIndex} );
-			
-			let firstTime = (playerPos.x === null && playerPos.y === null);
-			if (emoji === "🚪" && firstTime) {
-				playerPos.x = posX;
-				playerPos.y = posY;
-				game.fillText(emojis["PLAYER"], playerPos.x, playerPos.y);
-				console.log(showInGridMeasures(playerPos));
+
+			if (emoji === "🚪" && firstLoad) {
+				playerPos.x = (colIndex+1);
+				playerPos.y = (rowIndex+1);
+				game.fillText(emojis["PLAYER"], posX, posY);
+				console.log(`Jugador (${playerPos.x}, ${playerPos.y})`);
 			}
+			
+			if (emoji === "🎁") {
+				giftPos.x = (colIndex+1);
+				giftPos.y = (rowIndex+1);
+				firstLoad && console.log(`Regalito (${giftPos.x}, ${giftPos.y})`);
+			}
+			
+			
 		})
 	});
+	
+	if (playerMoved) {
+		const posX = (playerPos.x-1) * gridSize; // 0 ~ 9
+		const posY = playerPos.y * gridSize; // 1 ~ 10
+		game.fillText(emojis["PLAYER"], posX, posY);
+		
+		const wasTheLevelComplete = (playerPos.x === giftPos.x) && (playerPos.y === giftPos.y);
+		if (wasTheLevelComplete) {
+			actualLevel++;
+			
+			playerPos.x = null;
+			playerPos.y = null;
+			
+			giftPos.x = null;
+			giftPos.y = null;
+			
+			renderMap();
+		} 
+	}
 }
 
 function movePlayer(direction) {
-	function renderPlayer() {
-		renderMap();
-		game.fillText(emojis["PLAYER"], playerPos.x, playerPos.y);
-	}	
 	
 	switch(direction) {
 		case "up":
-			if ( (playerPos.y - gridSize) >= mapLimits.up ) {
-				playerPos.y -= gridSize;
-				renderPlayer();
+			if ( (playerPos.y - 1) >= MAP_LIMITS.UP ) {
+				playerPos.y--;
+				renderMap();
 			}
-			console.log(showInGridMeasures(playerPos));
 			break;
 			
 		case "left":
-			if ( (playerPos.x - gridSize) >= mapLimits.left) {
-				playerPos.x -= gridSize;
-				renderPlayer();
+			if ( (playerPos.x - 1) >= MAP_LIMITS.LEFT) {
+				playerPos.x--;
+				renderMap();
 			}
-			console.log(showInGridMeasures(playerPos));
 			break;
 			
 		case "right":
-			if ( (playerPos.x + gridSize) <= mapLimits.right) {
-				playerPos.x += gridSize;
-				renderPlayer();
+			if ( (playerPos.x + 1) <= MAP_LIMITS.RIGHT) {
+				playerPos.x++;
+				renderMap();
 			}
-			console.log(showInGridMeasures(playerPos));
 			break;
 			
 		case "down":
-			if ( (playerPos.y + gridSize) <= mapLimits.down) {
-				playerPos.y += gridSize;
-				renderPlayer();
+			if ( (playerPos.y + 1) <= MAP_LIMITS.DOWN) {
+				playerPos.y++;
+				renderMap();
 			}
-			console.log(showInGridMeasures(playerPos));
 			break;
 	}
+	
+	console.log(`Jugador (${playerPos.x}, ${playerPos.y})`);
 }
 
 function moveByClicks(clickEvent) {
@@ -174,11 +198,4 @@ function moveByKeys(keyEvent) {
 			// console.log("abajo");
 			break;
 	}
-}
-
-function showInGridMeasures(itemPosition) {
-	return {
-		x: (itemPosition.x / gridSize) + 1,
-		y: (itemPosition.y / gridSize)
-	};
 }
