179cf6ffc7b41a1dfd5178567303cdf0f5853f5f
Miguel Velásquez <vmiguel.xy@gmail.com>
2022-10-05 10:34:17 -0400

    Diseño Responsive y Menús

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 "use strict";
+const screen = document.querySelector(".game-screen-container");
 const timeShown = document.querySelector("#time");
 const livesShown = document.querySelector("#lives");
 const recordShown = document.querySelector("#record");
@@ -13,6 +14,8 @@ class ItemPos {
 }
 
 /* Initial Values */
+let theGameStarted = false;
+
 let canvasSize = null;
 let gridSize = null;
 let elementsSize = null;
@@ -27,9 +30,9 @@ let totalGameTime = 0;
 const lastLevel = maps.length - 1;
 let currentRecord = Number(localStorage.getItem("record"));
 
-timeShown.innerText = "-- -- --";
-livesShown.innerText = emojis["HEART"].repeat(currentLives);
-currentRecord ? recordShown.innerText = formatSeconds(currentRecord) : recordShown.innerText = "-- -- --";
+timeShown.innerText = "--";
+livesShown.innerText = "-- -- -- -- --";
+currentRecord ? recordShown.innerText = formatSeconds(currentRecord) : recordShown.innerText = "--";
 
 const MAP_LIMITS = {
 	UP: 1,
@@ -41,6 +44,7 @@ const MAP_LIMITS = {
 /* Event Listeners */
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
+window.addEventListener("load", showMainMenu);
 window.addEventListener("keydown", moveByKeys);
 document.getElementById("up").addEventListener("click", moveByClicks);
 document.getElementById("left").addEventListener("click", moveByClicks);;
@@ -50,6 +54,7 @@ document.getElementById("down").addEventListener("click", moveByClicks);;
 /* Game Logic */
 let currentTimer = null;
 function startTimer() {
+	timeShown.innerText = "";
 	currentTimer = setInterval( () => {
 		totalGameTime++;
 		const currentTime = formatSeconds(totalGameTime);
@@ -63,18 +68,17 @@ function setCanvasSize() {
 	} else {
 		canvasSize = window.innerWidth * 0.8;
 	}
-	renderMap();
-}
-
-function renderMap() {
+	
 	canvasSize = Math.trunc(canvasSize);
+	gridSize = Math.trunc(canvasSize / 10);
 	canvas.setAttribute("width", canvasSize);
 	canvas.setAttribute("height", canvasSize);
 	
-	gridSize = Math.trunc(canvasSize / 10);
-	elementsSize = gridSize;
-	// console.log( {canvasSize, gridSize, elementsSize} );
-	
+	if (theGameStarted) renderMap();
+}
+
+function renderMap() {	
+	elementsSize = gridSize;	
 	game.textAlign = "start"
 	game.font = `${elementsSize}px sans-serif`;
 	
@@ -102,7 +106,7 @@ function renderMap() {
 						// console.log(`Jugador (${playerPos.x}, ${playerPos.y})`);
 						break;
 						
-					case "🎁":
+					case "💾":
 						giftPos.x = (colIndex+1);
 						giftPos.y = (rowIndex+1);
 						// console.log(`Regalito (${giftPos.x}, ${giftPos.y})`);
@@ -129,34 +133,36 @@ function renderMap() {
 /* Player Movement */
 function movePlayer(direction) {
 	
-	switch(direction) {
-		case "up":
-			if ( (playerPos.y - 1) >= MAP_LIMITS.UP ) {
-				playerPos.y--;
-				renderMap();
-			}
-			break;
-			
-		case "left":
-			if ( (playerPos.x - 1) >= MAP_LIMITS.LEFT) {
-				playerPos.x--;
-				renderMap();
-			}
-			break;
-			
-		case "right":
-			if ( (playerPos.x + 1) <= MAP_LIMITS.RIGHT) {
-				playerPos.x++;
-				renderMap();
-			}
-			break;
-			
-		case "down":
-			if ( (playerPos.y + 1) <= MAP_LIMITS.DOWN) {
-				playerPos.y++;
-				renderMap();
-			}
-			break;
+	if (theGameStarted) {
+		switch(direction) {
+			case "up":
+				if ( (playerPos.y - 1) >= MAP_LIMITS.UP ) {
+					playerPos.y--;
+					renderMap();
+				}
+				break;
+				
+			case "left":
+				if ( (playerPos.x - 1) >= MAP_LIMITS.LEFT) {
+					playerPos.x--;
+					renderMap();
+				}
+				break;
+				
+			case "right":
+				if ( (playerPos.x + 1) <= MAP_LIMITS.RIGHT) {
+					playerPos.x++;
+					renderMap();
+				}
+				break;
+				
+			case "down":
+				if ( (playerPos.y + 1) <= MAP_LIMITS.DOWN) {
+					playerPos.y++;
+					renderMap();
+				}
+				break;
+		}
 	}
 	
 	// console.log(`Jugador (${playerPos.x}, ${playerPos.y})`);
@@ -223,50 +229,151 @@ function moveByKeys(keyEvent) {
 }
 
 /* Win and Lose Conditions */
-function wasTheLevelComplete() { // Win Condition
+function endGame(wonOrLost) {
+	theGameStarted = false;
+	switch (wonOrLost) {
+		case "won":
+			console.log("¡Ganaste el Juego!");
+			clearInterval(currentTimer);
+			game.clearRect(0,0, canvasSize, canvasSize);
+			showGameWonMenu();
+			
+			if (totalGameTime < currentRecord) {
+				currentRecord = totalGameTime;
+				localStorage.setItem("record", totalGameTime);
+				recordShown.innerText = formatSeconds(currentRecord);
+			}
+			break;
+		
+		case "lost":
+			console.log("Perdiste todas tus vidas: GAME OVER.");
+			game.clearRect(0,0, canvasSize, canvasSize);
+			clearInterval(currentTimer);
+			showGameLostMenu();
+			break;
+	}
+}
+
+// Win Condition
+function wasTheLevelComplete() {
 	const itWas = (playerPos.x === giftPos.x) && (playerPos.y === giftPos.y);
 	if (itWas) {
 		if (currentLevel !== lastLevel) {
-			console.log("Pasaste de Nivel~");
+			console.log("¡Pasaste de Nivel!");
 			resetAllPositions();
 			currentLevel++;
 			renderMap();
 		} else {
-			gameWon();
+			endGame("won");
 		}
 	}
 }
 
-function wasAnyBombHit() { // Lose Condition
+// Lose Condition
+function wasAnyBombHit() {
 	const itWas = bombsPos.find(bomb => ( (bomb.x === playerPos.x) && (bomb.y === playerPos.y) ));
 	if (itWas) {
 		currentLives--;
 		updateLivesShown();
-		if (currentLives === 0) gameLost();
-		resetAllPositions();
-		renderMap();
+		
+		if (currentLives > 0) {
+			console.log("Perdiste una vida.")
+			resetAllPositions();
+			renderMap();
+		} else {
+			endGame("lost");
+		}
 	}
 }
 
-function gameLost() {
-	console.log("Perdiste todas tus vidas. Empiezas desde el nivel 1");
+/* Menus */
+
+// Main Menu
+const mainMenuContainer = document.createElement("div");
+	mainMenuContainer.classList.add("game-menu");
+
+const mainMenuTitle = document.createElement("h2");
+	mainMenuTitle.innerText = "Simple Labyrinth";
+	mainMenuTitle.classList.add("game-menu__item");
+	mainMenuTitle.classList.add("game-menu__item--title");
+	
+const mainMenuBtn = document.createElement("button");
+	mainMenuBtn.innerText = "Jugar";
+	mainMenuBtn.classList.add("game-menu__item");
+	mainMenuBtn.classList.add("game-menu__item--btn");
+	mainMenuBtn.addEventListener("click", startGame);
+
+screen.appendChild(mainMenuContainer);
+mainMenuContainer.appendChild(mainMenuTitle);
+mainMenuContainer.appendChild(mainMenuBtn);
+function showMainMenu() { mainMenuContainer.classList.remove("inactive"); }
+function hideMainMenu() { mainMenuContainer.classList.add("inactive"); }
+
+// Win Menu
+const wonGameMenuContainer = document.createElement("div");
+	wonGameMenuContainer.classList.add("game-menu");
+	wonGameMenuContainer.classList.add("inactive");
+
+const wonGameMenuTitle = document.createElement("h2");
+	wonGameMenuTitle.innerText = "Ganaste";
+	wonGameMenuTitle.classList.add("game-menu__item");
+	wonGameMenuTitle.classList.add("game-menu__item--title");
+	
+const wonGameMenuTryAgainBtn = document.createElement("button");
+	wonGameMenuTryAgainBtn.innerText = "Jugar de nuevo";
+	wonGameMenuTryAgainBtn.classList.add("game-menu__item");
+	wonGameMenuTryAgainBtn.classList.add("game-menu__item--btn");
+	wonGameMenuTryAgainBtn.addEventListener("click", startGame);
+
+screen.appendChild(wonGameMenuContainer);
+wonGameMenuContainer.appendChild(wonGameMenuTitle);
+wonGameMenuContainer.appendChild(wonGameMenuTryAgainBtn);
+function showGameWonMenu() { wonGameMenuContainer.classList.remove("inactive"); }
+function hideGameWonMenu() { wonGameMenuContainer.classList.add("inactive"); }
+
+// Lose Menu
+const lostGameMenuContainer = document.createElement("div");
+	lostGameMenuContainer.classList.add("game-menu");
+	lostGameMenuContainer.classList.add("inactive");
+
+const lostGameMenuTitle = document.createElement("h2");
+	lostGameMenuTitle.innerText = "Perdiste";
+	lostGameMenuTitle.classList.add("game-menu__item");
+	lostGameMenuTitle.classList.add("game-menu__item--title");
+	
+const lostGameMenuTryAgainBtn = document.createElement("button");
+	lostGameMenuTryAgainBtn.innerText = "Volver a Intentarlo";
+	lostGameMenuTryAgainBtn.classList.add("game-menu__item");
+	lostGameMenuTryAgainBtn.classList.add("game-menu__item--btn");
+	lostGameMenuTryAgainBtn.addEventListener("click", startGame);
+
+screen.appendChild(lostGameMenuContainer);
+lostGameMenuContainer.appendChild(lostGameMenuTitle);
+lostGameMenuContainer.appendChild(lostGameMenuTryAgainBtn);
+function showGameLostMenu() { lostGameMenuContainer.classList.remove("inactive"); }
+function hideGameLostMenu() { lostGameMenuContainer.classList.add("inactive"); }
+
+// Menu Functions
+function startGame() {
+	hideMainMenu();
+	hideGameWonMenu();
+	hideGameLostMenu();
+	
+	theGameStarted = true;
+	totalGameTime = 0;
 	currentLevel = 0;
 	currentLives = 5;
-	livesShown.innerText = emojis["HEART"].repeat(currentLives);
-}
-
-function gameWon() {
-	console.log("¡Ganaste el Juego!");
-	clearInterval(currentTimer);
+	resetAllPositions();
 	
-	if (totalGameTime < currentRecord) {
-		currentRecord = totalGameTime;
-		localStorage.setItem("record", totalGameTime);
-		recordShown.innerText = formatSeconds(currentRecord);
-	}
+	showPlayerLives();
+	startTimer();
+	renderMap();
 }
 
 /* Helpers */
+function showPlayerLives() { livesShown.innerText = emojis["HEART"].repeat(currentLives); }
+function updateLivesShown() { livesShown.innerText = emojis["HEART"].repeat(currentLives); }
+
 function resetAllPositions() {
 	playerPos.x = null;
 	playerPos.y = null;
@@ -275,10 +382,6 @@ function resetAllPositions() {
 	giftPos.y = null;
 	
 	bombsPos = [];
-}
-
-function updateLivesShown() {
-	livesShown.innerText = emojis["HEART"].repeat(currentLives);
 }
 
 function formatSeconds(timeInSeconds) {
